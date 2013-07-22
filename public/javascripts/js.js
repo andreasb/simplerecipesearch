03867dc39f9305fc353f5f35994c00409a56341c
andreasb <andreasbirkebaek@gmail.com>
2013-07-22 20:55:08 +0200

    now loads more recipes on click

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -1,18 +1,23 @@
-var socket = io.connect('http://localhost');
-// Keep a count of the recipes that are displayed
+var socket = io.connect(window.location.origin);
 var currentCount = 100;
-
 var path = window.location.pathname;
 var search_term = path.substr(1, path.lastIndexOf('/') - 1);
 
-// Call this when a button for more recipes is pressed
-socket.emit('requestForMoreRecipes', {
-	search_term: search_term,
-	recipes_displayed: currentCount
-});
+// When the 'more' button is pressed, this buttom is called
+function getMoreRecipes () {
+	socket.emit('requestForMoreRecipes', {
+		search_term: search_term,
+		recipes_displayed: currentCount
+	});
+};
 
 // Load the recipes in the view when they are recieved
 socket.on('moreRecipes', function (data) {
-	console.log(data.recipes);
+	var recipe;
+	for (i in data.recipes) {
+		recipe = data.recipes[i];
+		var insertAt = currentCount - 1;
+		$('#list li:nth-child(' + insertAt + ')').after('<li class="recipelist"><div class="result"><a href=""><span>' + recipe.name + '</span></a></div></li>');
+	};
 	currentCount += 100;
 });
