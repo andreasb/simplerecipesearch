63c6a7efbb71b623e1c5f7f2166ff86869122cc7
andreasb <andreasbirkebaek@gmail.com>
2013-07-23 12:16:07 +0200

    add heroku socket configs

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
+io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+});
+
 io.set('log level', 1);
 
 console.log("Express server listening on port " + port);
